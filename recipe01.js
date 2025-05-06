508560446becfb7047d90f0ad06a398523f226c2
Janna <ycoyjannalynnunez@gmail.com>
2025-05-06 22:40:13 +0800

    recipe01

---
+++ recipe01.js
@@ -0,0 +1,40 @@
+let intervals = [];
+
+function startTimer(button) {
+  const timerContainer = button.parentElement;
+  const timeDisplay = timerContainer.querySelector(".time");
+  let time = 0;
+
+  if (timerContainer.dataset.index === undefined) {
+    timerContainer.dataset.index = intervals.length;
+    intervals.push(null);
+  }
+
+  const index = timerContainer.dataset.index;
+
+  if (intervals[index]) return; 
+  intervals[index] = setInterval(() => {
+    time++;
+    const minutes = String(Math.floor(time / 60)).padStart(2, '0');
+    const seconds = String(time % 60).padStart(2, '0');
+    timeDisplay.textContent = `${minutes}:${seconds}`;
+  }, 1000);
+
+  timerContainer.dataset.time = time;
+}
+
+function pauseTimer(button) {
+  const timerContainer = button.parentElement;
+  const index = timerContainer.dataset.index;
+  clearInterval(intervals[index]);
+  intervals[index] = null;
+}
+
+function resetTimer(button) {
+  const timerContainer = button.parentElement;
+  const timeDisplay = timerContainer.querySelector(".time");
+  const index = timerContainer.dataset.index;
+  clearInterval(intervals[index]);
+  intervals[index] = null;
+  timeDisplay.textContent = "00:00";
+}
